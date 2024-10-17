c99f597f2428616d892ec11796b2efa5b51bac87
Kristoffer Hansson <97549161+Zarser@users.noreply.github.com>
2024-10-18 01:29:09 +0200

    Version 1

--- obj/Debug/net8.0/DinMinnesBank.AssemblyInfo.cs
+++ obj/Debug/net8.0/DinMinnesBank.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DinMinnesBank")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4dc1729da0778a4642547a8f666aaa40fdc715fa")]
 [assembly: System.Reflection.AssemblyProductAttribute("DinMinnesBank")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DinMinnesBank")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
